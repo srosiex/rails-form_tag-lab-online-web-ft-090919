4765bb2c86b4e068ea12d33fe249a20c489270a2
Sarah Henderson <sarahrose.henderson@gmail.com>
2019-11-07 09:45:58 -0600

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -4,17 +4,23 @@ class StudentsController < ApplicationController
     @students = Student.all
   end
 
-  def show
-    @student = Student.find(params[:id])
-  end
 
   def new
+    @student = Student.new
+    render :new
   end
+
+
+
 
   def create
    student =  Student.new({first_name: params[:first_name], last_name: params[:last_name]})
     student.save
     redirect_to student
+  end
+  def show
+    @student = Student.find(params[:id])
+    render :show
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # For details on the DSL available within this file, see http://guides  .rubyonrails.org/routing.html
   resources :students, only: :index
-  get '/students/:id', to: 'students#show', as: 'student'
-  post '/students', to: 'students#create', as: 'create_student'
+  get '/students', to: 'students#index'
   get '/students/new', to: 'students#new', as: 'new_student'
+  post '/students', to: 'students#create', as: 'create_student'
+  get '/students/:id', to: 'students#show', as: 'student'
+
 end
